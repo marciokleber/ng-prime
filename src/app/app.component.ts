3a4ea4f248249b405bd775d316ddad7f30b69d66
Marcio Kleber Junior Lopes de Souza <marciokjls@gmail.com>
2024-06-10 09:35:40 -0300

    Ajuste na estrutura do projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  {{ title }}
+  <router-outlet></router-outlet>
+  `,
 })
-export class AppComponent {
-  title = 'app-prime-ng';
+export class AppComponent implements OnInit{
+  title = 'ng-prime';
+
+  ngOnInit(): void{
+    console.log('NgOnInit');
+  }
 }
